29cecb083aa1eb4c2824df95780dd6fa927dcf18
race-briggs <race.briggs@gmail.com>
2019-09-19 13:23:49 -0500

    made successful api call to Edamam

--- assets/javascript/webapp.js
+++ assets/javascript/webapp.js
@@ -1,26 +1,25 @@
 $(document).ready(function(){
 
 function callEdamam(query){
-  var queryURL = "ttps://api.edamam.com/search?";
+  var queryURL = "https://api.edamam.com/search?";
 
   var q = query;
 
-  var apiId = "api_id=6b37a04b";
+  var apiId = "app_id=6b37a04b";
 
-  var apiKey = "api_key=59ee4eaaea79bef6ea234639a9c48a1a";
+  var apiKey = "app_key=59ee4eaaea79bef6ea234639a9c48a1a";
 
-  queryURL = queryURL + q + "&" + apiId + "&" + apiKey;
+  queryURL = queryURL + "q=" + q + "&" + apiId + "&" + apiKey;
 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response){
-    console.log(response)
-
-  })
-
-    
+    console.log(response);
+  })   
 }
+
+callEdamam("chicken");
 
 
 });
