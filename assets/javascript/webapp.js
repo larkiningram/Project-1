7285672a3e969b678f0b7e5a70a1b7482a3c7d13
Sager Williams <sagerwilliams0011@gmail.com>
2019-09-23 14:22:56 -0500

    Changed opacity of bar graph.

--- assets/javascript/webapp.js
+++ assets/javascript/webapp.js
@@ -255,9 +255,9 @@ function renderCharts() {
       datasets: [{
         data: [protien, carbs, fat],
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)'
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)'
         ],
         borderColor: [
           'rgba(255, 99, 132, 1)',
